17e98de309ce3fb661a849daeba1cf1d541621f1
IkerGutierrez1 <173085374+IkerGutierrez1@users.noreply.github.com>
2024-12-05 10:51:11 +0100

    Code cleaned

--- analysisi.R
+++ analysisi.R
@@ -10,17 +10,14 @@ path <- "data/df_RoomT.csv"
 
 df <- load_data(path)
 
-df_no_time <- df[-1,-1] #Eliminar columna de NAs
+df_no_time <- df[-1,-1] #Delte col with NA
 cols <- names(df_no_time)
 cols <- sub("\\.Sensor.*", "", cols)
 
-#Test
+
 X <- as.matrix(df_no_time)
-
-Xt <- t(X)
-
 X_centered <- scale(X, center = TRUE, scale = FALSE)
-X_standar <- scale(X)
+X_standar <- scale(X) #Not used
 
 media <- attr(X_standar, "scaled:center")
 desviacion_estandar <- attr(X_standar, "scaled:scale")
@@ -28,9 +25,9 @@ desviacion_estandar <- attr(X_standar, "scaled:scale")
 result_SVD <- svd(X_centered)
 
 
-U <- result_SVD$u #vecotores propios a la izquierda (espacio de observaciones 7391x6)
-D <- result_SVD$d #Valor absoluto de valores propios (valor propio) #Singular values
-V <- result_SVD$v #vectores propios a la derecha (espacio de vatriables 6x6)
+U <- result_SVD$u #Left singular vec
+D <- result_SVD$d #Singular values
+V <- result_SVD$v #Right singular vec
 
 
 
@@ -43,7 +40,7 @@ eigen_values <- ((D)^2)/(n-1)
 W <- numeric(m)
 Y <- matrix(0, nrow = k, ncol = m)
 
-#Se podira hacer en un solo ciclo probablemente
+#Method 1 to calc spectrum
 for (i in 1:k){
   wkk <- 0
   for (j in 1:m) {
@@ -75,37 +72,31 @@ for (i in 1:k){
     Y[i,j] <- ykm
   }
 }
-colors <- c("#4C4C8A", "#6A6A9A", "#7F7FB3", "#A3A3C2", "#B2B2D3", "#C2C2E6")
-#colors <- c("#C45A0D", "#A02020", "#4A90E2", "#4E9F3D", "#D4AC0E", "#8B4513")
-#png("outputs/SVD_Spectrum.png", width = 1080, height = 720)
-png("SVD_Spectrum.png", width = 1080, height = 720)
 
-# Crear un barplot
+#Plot spectrum
+colors <- c("#4C4C8A", "#6A6A9A", "#7F7FB3", "#A3A3C2", "#B2B2D3", "#C2C2E6")
+png("SVD_Spectrum.png", width = 1080, height = 720)
 barplot(Y, beside = TRUE, 
-        col = colors,  # Cambia el color según tus preferencias
+        col = colors, 
         xlab = "Principal components",
         ylab = "SVD-Spectrum",
-        names.arg = 1:6,  # Etiquetas en el eje x
-        ylim = c(0, 1),  # Establece el rango del eje y
+        names.arg = 1:6,  
+        ylim = c(0, 1), 
         axes = FALSE,
-        legend.text = TRUE)  # Agrega leyenda si lo deseas
-
+        legend.text = TRUE)  
 axis(2, at = seq(0, 1, by = 0.1), las = 1)
 grid()
-# Agregar leyenda
-legend("topright",  # Posición de la leyenda (puedes cambiar a "topleft", "bottomright", etc.)
-       legend = cols,  # Nombres correspondientes
+legend("topright", 
+       legend = cols,  
        fill = colors,
-       #cex = 1,
-       #text.width = 0.6,
-       cex = 1.2,  # Aumenta el tamaño del texto
-       text.width = strwidth(max(cols)) * 1.2,  # Aumenta el ancho de la caja
+       cex = 1.2,  
+       text.width = strwidth(max(cols)) * 1.2, 
        box.lwd = 0.5,
        bg = "lightgray")  
 
 dev.off()
 
-#Cumulative PDF
+#Cumulative PDF plots
 pdf("outputs/cpgrams.pdf")
 
 for(col in cols){
@@ -127,49 +118,45 @@ for(col in cols){
 
 
 
-
-
-#Otra forma de calucular
-dfxdfT <- t(as.matrix(df_no_time)) %*% (as.matrix(df_no_time))
-vp <- eigen(dfxdfT) #La raiz de estos correponde con D
-
-
-
-# Inicializa un data.frame vacío para almacenar los datos
+#Df to save data
 combined_data <- data.frame()
 
 for (col in cols) {
   column <- paste0(col, ".Sensor__room_temperature")
   
-  # Agrega los datos al data.frame combinado
+  #Add data to df
   temp_data <- df_no_time[[column]]
   combined_data <- rbind(combined_data, data.frame(Temperatura = temp_data, Column = column))
 }
 
-png("outputs/density.png", width = 1080, height = 720)
 
-# Crea el gráfico con todas las curvas
+#Density plot
+png("outputs/density.png", width = 1080, height = 720)
 ggplot(combined_data, aes(x = Temperatura, color = Column)) +
   geom_density() + 
   labs(title = "T Density", x = "Temperature", y = "Density") +
   theme_minimal()
-# Cierra el archivo PDF
 dev.off()
 
 
 
 
 #Reconstruct original
-#Los 6 componentes principales (Los dos metodos tienen que ser lo mismo)
 k <- 1
 S <- diag(D)
 Sk <- S[1:k,1:k]
 Uk <- U[,1:k]
-principal <- Uk%*%Sk #Si k = 1 no funciona multiplicar un escalar  (componente principal)
-principal <- Uk*Sk
 
-T_build <- as.matrix(principal)+sum(media)/length(media) #Forma de conseguir una temperatura
+if (k != 1) {
+  principal <- Uk %*% Sk  # Matricial prodcut k no 1
+} else {
+  principal <- Uk * Sk   # PProduct k 1
+}
 
+#Add mean to obtain temperature measurments 
+T_build <- as.matrix(principal)+sum(media)/length(media) 
+
+#Reconstruction of all signals
 Vk <- t(V)[1:k,]
 Xk <- as.matrix(principal)%*%Vk 
 
@@ -177,21 +164,23 @@ Xk <- as.matrix(principal)%*%Vk
 Xk <- Xk+media
 
 
-#Move to dataframe whith timestamps
+#Move to dataframe whith timestamps for plot
 Xk_df <- data.frame(Xk)
 Xk_mean <- data.frame(T_build = rowMeans(Xk_df[,1:6]))
 names(Xk_df) <- cols
+
 Xk_df$timestamp <- df$timestamp[-1]
 Xk_mean$timestamp <- df$timestamp[-1]
+
 T_build_df <- data.frame(T_build)
 T_build_df$timestamp <- df$timestamp[-1]
 
-#Scatter de Xk_df y X para comparar las reconstrucciones
+
 #Save dfs
 write.csv2(T_build_df, "k1_T_build.csv", row.names = FALSE)
 
 
-# Añadir una columna para identificar el origen de los datos
+#Prepare df for ploting
 df_graph <- df[-1,]
 
 colnames(df_graph) <- paste("df_",colnames(df_graph), sep = "")
@@ -207,7 +196,8 @@ df_filtered$timestamp <- as.POSIXct(df_filtered$timestamp, format = "%Y-%m-%d %H
 
 
 
-#All plots
+#All plots of reconstruction for all rooms for all principal components and scatter plost of reconstructed
+#real data
 base_save_dir = "outputs/plots/SWD_standarized/"
 is_standarized = TRUE
 is_centered = FALSE
@@ -232,14 +222,13 @@ for (k in 1:6){
   Sk <- S[1:k,1:k]
   Uk <- U[,1:k]
   if (k != 1){
-    principal <- Uk%*%Sk} #Si k = 1 no funciona multiplicar un escalar  (componente principal)}
+    principal <- Uk%*%Sk} 
   else{
     principal <- Uk*Sk}
   
   Vk <- t(V)[1:k,]
   Xk <- as.matrix(principal)%*%Vk 
   
-  #Linea solo si no se ha estandarizado al principio
   if (is_standarized){
     Xk <- Xk*desviacion_estandar+media}
   
@@ -251,7 +240,6 @@ for (k in 1:6){
   names(Xk_df) <- cols
   Xk_df$timestamp <- df$timestamp[-1]
   
-  #No hace falta que se ejecute en cada ciclo
   df_graph <- df[-1,]
   
   colnames(df_graph) <- paste("df_",colnames(df_graph), sep = "")
@@ -265,7 +253,7 @@ for (k in 1:6){
   #Scatter
   file_path = paste0(save_dir,"scatter.PNG")
   p <- ggplot(df_combined, aes(x = df_RoomB.Sensor__room_temperature, y = RoomB.Sensor__room_temperature)) +
-    geom_point() +  # Esta capa agrega los puntos al gráfico
+    geom_point() +  
     labs(
       x = "Original",
       y = "SWD"
@@ -292,41 +280,34 @@ for (k in 1:6){
       colors <- c(col = "red", 
                   original_col = "blue")
       
-      # Cambiar los nombres de las columnas (que están como strings) por sus valores dinámicos
       names(colors) <- c(col, original_col)
       
       
-      # Crear la gráfica
       g <- ggplot(df_long, aes(x = timestamp, y = Temperature, color = Source)) + 
         geom_point(size = 2, alpha = 0.7) +
         geom_line() +
-        
-        # Título y etiquetas
+      
         labs(
           title = col,
           x = "Timestamp",
           y = "Temperature (°C)"
         ) +
         
-        # Personalización de la apariencia
-        theme_bw() +  # Fondo blanco
+        theme_bw() +  
         theme(
           axis.text.x = element_text(angle = 45, hjust = 1, size = 10),  
           axis.title = element_text(size = 12, face = "bold"),  
           plot.title = element_text(size = 14, face = "bold", hjust = 0.5), 
         ) +
         
-        # Ajustar las fechas del eje X
         scale_x_datetime(date_breaks = date_breaks, date_labels = "%Y-%m-%d %H:%M") +
         
-        # Limitar el eje Y entre 18 y 30
         ylim(18, 30) +
         
         scale_color_manual(values = colors, 
                            labels = c("Original", "SWD"))
       
       
-      # Guardar la gráfica como archivo PNG
       ggsave(file_path, plot = g, width = 8, height = 6, dpi = 300)
       
   }}}
@@ -335,7 +316,7 @@ for (k in 1:6){
 
 
 
-#Write errors
+#Calculte and save residuals
 fileConn <- file("results.txt", open = "w")
 
 writeLines("Ciclo\t\tRoom\tMAE\tRMSE", con = fileConn)
@@ -347,14 +328,13 @@ for (k in 1:6){
   Sk <- S[1:k,1:k]
   Uk <- U[,1:k]
   if (k != 1){
-    principal <- Uk%*%Sk} #Si k = 1 no funciona multiplicar un escalar  (componente principal)}
+    principal <- Uk%*%Sk} 
   else{
     principal <- Uk*Sk}
     
   Vk <- t(V)[1:k,]
   Xk <- as.matrix(principal)%*%Vk 
   
-  #Linea solo si no se ha estandarizado al principio
   if (is_standarized){
   Xk <- Xk*desviacion_estandar+media}
 
@@ -393,11 +373,11 @@ for (col in colnames){
     labs(title = "Real vsd PCA",
          x = "Real measurment",
          y = "Reconstructed by PCA") +
-    theme_bw() +  # Aplicar tema con fondo blanco
+    theme_bw() +  
     theme(
-      plot.background = element_rect(fill = "white"), # Fondo blanco para todo el gráfico
-      panel.grid.major = element_line(color = "grey90"), # Líneas de cuadrícula más suaves
-      panel.grid.minor = element_blank()               # Sin líneas menores
+      plot.background = element_rect(fill = "white"),
+      panel.grid.major = element_line(color = "grey90"), 
+      panel.grid.minor = element_blank()               
     )
   
   file_name <- paste0("outputs/scatter/scatter_", col, ".png")
@@ -411,14 +391,13 @@ df_long <- df %>%
 df_filtered <- df_long %>%
   filter(timestamp >= as.POSIXct("2023-11-13 00:00:00") & timestamp <= as.POSIXct("2023-11-13 23:59:59"))
 
-# Crear el gráfico
+
 p <- ggplot(df_filtered, aes(x = timestamp, y = value, color = variable)) +
-  geom_point() +  # Usar geom_point() si prefieres puntos en lugar de líneas
+  geom_point() +  
   labs(title = "Valores de las columnas frente a timestamp",
        x = "Timestamp",
        y = "Valor") +
   theme_minimal() +
-  theme(legend.title = element_blank())  # Eliminar el título de la leyenda
+  theme(legend.title = element_blank()) 
 
-# Mostrar el gráfico
 print(p)
