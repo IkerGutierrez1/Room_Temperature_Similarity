19e6bfd49cf216a81675c1fe2b7801325c723729
IkerGutierrez1 <173085374+IkerGutierrez1@users.noreply.github.com>
2024-12-11 13:30:08 +0100

    Corrected comments

--- functions.R
+++ functions.R
@@ -19,12 +19,8 @@ load_data <- function(path){
 interpolate_NA <- function(df){
   df <- df %>%
     mutate(across(-timestamp, ~ as.numeric(as.character(.))))
-  
-  #Función para hacer una interpolación de los datos que tenga NA, asumen que timestamp
-  #esta correctamente preprocesado
   col_NA <- colSums(is.na(df))
   print(col_NA)
-  #Hay 13 valores de yTi que son NA y 195 de Te
   for(col in names(col_NA)){
     if(col_NA[col] > 0){
       print(paste("Columna:", col, "- Interpolando", col_NA[col], "valores NA"))
@@ -32,10 +28,7 @@ interpolate_NA <- function(df){
         mutate(!!col := na.approx(.[[col]], na.rm = FALSE))
     }
   }
-  
-  # Eliminar filas donde todas las columnas (excepto 'timestamp' y 't') son NA, 
-  #la interpolacion no funciona si no hay valoresm validos antes de NA, por lo que
-  #los NA de la primera hora del dataset se eliminan aqui
+
   df <- df %>%
     filter(!rowSums(is.na(select(., -c(timestamp)))) == (ncol(df) - 2))
   return (df)
